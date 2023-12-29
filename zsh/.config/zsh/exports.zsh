8bb4a5b500524b15567d3b1c3832bec2f354d855
Alex <seremet.alexandru.work@gmail.com>
2023-12-29 12:15:52 +0200

    update nvim, alacritty, tmux and zsh config

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -18,3 +18,9 @@ export PERL_MB_OPT
 PERL_MM_OPT="INSTALL_BASE=/home/alex/perl5"
 export PERL_MM_OPT
 export PATH="/usr/pgsql-14/bin:$PATH"
+
+# Load nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# This loads nvm bash_completion
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
